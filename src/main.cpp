7e83189b9bd389cbc74a18ede096185905a82eea
Rafau-18 <rafal.urbaniak@gmail.com>
2020-05-07 01:18:31 +0200

    Pozycje szachownicy

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,17 @@
 // Piny wejściowe i wyjsciowe macierzy 8x8 dla szachownicy 
 int tab_out[8]={13,12,14,27,26,25,33,32};
 int tab_read[8]={15,22,4,16,17,5,18,19};
+void odczyt();
+void initialize();
+const char* TablicaPozycji[8][8]={{"A1","A2","A3","A4","A5","A6","A7","A8"},
+                                  {"B1","B2","B3","B4","B5","B6","B7","B8"},
+                                  {"C1","C2","C3","C4","C5","C6","C7","C8"},
+                                  {"D1","D2","D3","D4","D5","D6","D7","D8"},
+                                  {"E1","E2","E3","E4","E5","E6","E7","E8"},
+                                  {"F1","F2","F3","F4","F5","F6","F7","F8"},
+                                  {"G1","G2","G3","G4","G5","G6","G7","G8"},
+                                  {"H1","H2","H3","H4","H5","H6","H7","H8"},
+                                 };
 //----------------
 
 void setup() {
@@ -53,19 +64,16 @@ void initialize(){
 
 void odczyt(){
     
-  for (int j=2;j<3;j++){
-
-    for(int i=0;i<8;i++){
-      digitalWrite(tab_out[i],HIGH);
-    }
-    delay(50);
-    Serial.print("Kolumna: "); Serial.println(j);    
-    digitalWrite(tab_out[j],LOW);
-    delay(50);
-    for(int l=0;l<7;l++){
-      Serial.print(digitalRead(tab_read[l]));  
+  for (int j=0;j<8;j++){
+    
+     digitalWrite(tab_out[j],LOW); // ustawienie kolumny w stanie niskim do odczytu        
+    
+    for(int l=0;l<8;l++){
+      //Serial.print(digitalRead(tab_read[l]));  
       delay(50);    
     }
-    Serial.println(digitalRead(tab_read[7]));
+        
+    digitalWrite(tab_out[j],LOW); // ustawienie kolumny w stanie niskim do odczytu
   }
 }
+
