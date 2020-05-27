121f042f0ef1267ea70e8c57894286eb68f08ef8
Rafau-18 <rafal.urbaniak@gmail.com>
2020-05-27 04:19:11 +0200

    Wifi connection + Firebase connection + Firebase live status on chessboard

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,61 @@
 #include <Arduino.h>
 
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+
+//1. Change the following info
+#define FIREBASE_HOST "chessboard-iot2020.firebaseio.com"
+#define FIREBASE_AUTH "whgvLkMDEeQ7ej6ur0ZBSfB1Mgx8a1vc13HbRRxZ"
+#define WIFI_SSID "Orange_Swiatlowod_09E0"
+#define WIFI_PASSWORD "GQKH6GTEKWLC"
+
+//const char* password =  "GQKH6GTEKWLC";
+
+
+
 // Piny wejściowe i wyjsciowe macierzy 8x8 dla szachownicy 
-int tab_out[8]={13,12,14,27,26,25,33,32};
+//int tab_out[8]={13,12,14,27,26,25,33,32};
 //int tab_read[8]={15,22,4,16,17,5,18,19};
-int tab_read[8]={19,18,5,17,16,4,23,15};
+//int tab_out[8]={19,18,5,17,16,4,2,15};
+int tab_out[8]={15,2,4,16,17,5,18,19};
+int tab_read[8]={13,12,14,27,26,25,33,34};
 void odczyt();
 void initialize();
 void wypisz();
 
-const char* TablicaPozycji[8][8]={{"A1","B1","C1","D1","E1","F1","G1","H1"},
+const String TablicaPozycji[8][8]={{"A1","B1","C1","D1","E1","F1","G1","H1"},
                                   {"A2","B2","C2","D2","E2","F2","G2","H2"},
                                   {"A3","B3","C3","D3","E3","F3","G3","H3"},
                                   {"A4","B4","C4","D4","E4","F4","G4","H4"},
                                   {"A5","B5","C5","D5","E5","F5","G5","H5"},
                                   {"A6","B6","C6","D6","E6","F6","G6","H6"},
                                   {"A7","B7","C7","D7","E7","F7","G7","H7"},
-                                  {"A8","B8","C8","D8","E8","F8","G8","H8"},                                                         
+                                  {"A8","B8","C8","D8","E8","F8","G8","H8"}                                                         
                                  };
 
-int PreviousState[8][8]; //Tablica z poprzednim odczytem
+int PreviousState[8][8]={{1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1}                                                                                        
+                        }; //Tablica z poprzednim odczytem
 int state[8][8]; // aktualnie odcztywany stan do porównania z poprzednim z tablicy
-int reading[8][8]; // 
+int reading[8][8]=      { {1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,1,1}}; // 
 
 unsigned long timer = 10; 
 unsigned long debounceDelay = 75; 
 unsigned long lastDebounceTime[8][8];// czas debouncingu na odczycie kontaktronu 
-
+FirebaseData standane;
 
 void setup() {
   
@@ -68,7 +98,7 @@ void initialize(){
     pinMode(tab_read[4],INPUT_PULLUP);
     pinMode(tab_read[5],INPUT_PULLUP);
     pinMode(tab_read[6],INPUT_PULLUP);
-    pinMode(tab_read[7],INPUT_PULLUP);    
+    pinMode(tab_read[7],INPUT);    
     digitalWrite(tab_out[0],HIGH);
     digitalWrite(tab_out[1],HIGH);
     digitalWrite(tab_out[2],HIGH);
@@ -77,7 +107,27 @@ void initialize(){
     digitalWrite(tab_out[5],HIGH);
     digitalWrite(tab_out[6],HIGH);
     digitalWrite(tab_out[7],HIGH);    
-    delay(1500);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to Wi-Fi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(300);
+    }
+      Serial.println();
+      Serial.print("Connected with IP: ");
+      Serial.println(WiFi.localIP());
+      Serial.println();
+      Serial.print(WIFI_SSID);
+
+      Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  //4. Enable auto reconnect the WiFi when connection lost
+      Firebase.reconnectWiFi(true);
+
+    delay(4500);
+
+
     Serial.println("Setup done");
 
     for (int i=0;i<8;i++){
@@ -86,13 +136,23 @@ void initialize(){
     
       for(int j=0;j<8;j++)
       {
-        reading[i][j]= digitalRead(tab_read[j]);
-        state[i][j]= reading[i][j];
+         reading[i][j]= digitalRead(tab_read[j]);
+         state[i][j]= reading[i][j];
+        // state[i][j]= digitalRead(tab_read[j]);
         PreviousState[i][j]=reading[i][j];
+        if(digitalRead(tab_read[j]))
+        {
+          Firebase.setBool(standane, "Status/"+ TablicaPozycji[i][j], 0);
+        }
+        else
+        {
+          Firebase.setBool(standane, "Status/"+ TablicaPozycji[i][j], 1);
+        }
       }
 
-      digitalWrite(tab_out[i],LOW);
+      digitalWrite(tab_out[i],HIGH);
     }
+     wypisz();
 
     Serial.println("Wczytano początkowe ustawienie");
     
@@ -119,15 +179,19 @@ void odczyt(){
 
           if(state[i][j]==0){
             Serial.println("Postawiono figurę na pozycji: " ); Serial.println(TablicaPozycji[i][j]);
+            Firebase.setBool(standane, "Status/"+ TablicaPozycji[i][j],1);
+            if(i==0 && j==0)
+            {
+              //Firebase.setBool(standane, "Status/A2", 1);
+            }
           } 
           else
           {
              Serial.println("Podniesiono figurę z pozycji: " ); Serial.println(TablicaPozycji[i][j]);
+            Firebase.setBool(standane, "Status/"+ TablicaPozycji[i][j], 0);
           }
           wypisz();
-        }
-
-          
+        }          
           
       } 
       PreviousState[i][j]=reading[i][j]; // zapisz odczyt jako stan poprzedni        
@@ -140,9 +204,9 @@ void odczyt(){
 
 void wypisz()
 {
-  for(int i =0; i<8 ; i++)
+  for(int i =7; i>=0 ; i--)
   {
-    if(i==0)
+    if(i==7)
     {
         
         Serial.println();
@@ -168,7 +232,7 @@ void wypisz()
       }
       
     }
-    if(i==7)
+    if(i==0)
     {
         Serial.println();
         //Serial.println("________________");
